b58c0695d9729e517a46ecc875c5191023e941e0
Jure Bernava Prah <jure.bernavaprah@exelero.com>
2021-12-02 17:47:22 +0100

    disabling testing on github

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/email/verify', function (Request $request) {
-    // Here only to generate correctly the path for verification.
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
 
 Route::get('/{any}', function () {
     return view('welcome');
